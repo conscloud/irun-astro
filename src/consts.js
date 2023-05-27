ba3df60c256925fea06ee61f298d2d5634e7864b
zhangjianyun <consc@foxmail.com>
2023-05-27 10:59:31 +0800

    更新文章2篇

--- src/consts.js
+++ src/consts.js
@@ -1,11 +1,11 @@
-export const SITE_TITLE = `Air Blog`;
-export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_TITLE = `ZhJy的随笔`;
+export const SITE_DESCRIPTION = '嗯，不知道要写些什么🤔';
+export const SITE_EMAIL = 'conscloud@gmail.com'
+export const SITE_NAME = 'www.okko.tk';
+export const SITE_URL = "https://www.okko.tk";
 export const SITE_LANG = "zh-CN";
 
 // Author, used only when author is not specified on the page, 
 // has lower priority than the author specified in the md file.
 // 作者信息，仅用在文章没有指定作者的时候，优先显示在 md 文件中指定的作者。
-export const SITE_AUTHOR = "Austin";
+export const SITE_AUTHOR = "ZhJy";
